59b54fd47b629cefe98b7979cae7165ba2c8a8fa
khristova21 <42977135+khristova21@users.noreply.github.com>
2018-09-19 23:05:29 -0400

    Add files via upload

---
+++ src/StringApiHomework.java
@@ -0,0 +1,31 @@
+
+public class StringApiHomework 
+{
+	public static void main(String[] args)
+	{
+		String river = "Mississippi";
+		
+		//length of word
+		System.out.println(river.length());
+		
+		//all caps of hello world
+		String hello = "Hello World";
+		System.out.println(hello.toUpperCase());
+		
+		//river.println();
+		//println is undefined for the variable String (error)
+		
+		//arguments are 'p' and 's'
+		//the method replace changes every letter P into an S
+		System.out.println(river.replace("p", "s"));
+		
+		System.out.println(river.toUpperCase());
+		
+		
+		
+		
+		
+	}
+	
+	
+}
